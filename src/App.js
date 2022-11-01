8f45f4485bd1ad7a77aaac7828a66f3d9051b483
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-01 15:40:08 +0300

    project start and bootstrap finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 mx-auto mt-4">
+            <h2 className="text-center text-white display-3">TÜRKİYE  COVID-19 Arama Motoru</h2>
+            <input type="text" placeholder="GG/AA/YY" className="form-control" />
+            <table class="table text-white table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Test Sayısı</th>
+                  <th scope="col">Hasta Sayısı</th>
+                  <th scope="col">Vefat Sayısı</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="text-white">
+                  <th scope="row">1</th>
+                  <td>13600</td>
+                  <td>2500</td>
+                  <td>20</td>
+                </tr>             
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
